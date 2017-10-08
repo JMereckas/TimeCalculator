f789710dc7c6c266980a09d02435ccff8dfd8c28
Justas <justasmereckas@gmail.com>
2017-10-08 16:39:23 +0100

    Added new route

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "hwllo Justas "
+
+@app.route('/clockin/<abc>')
+def clockin(abc):
+    return abc
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
