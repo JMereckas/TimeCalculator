5c0cacce98e0b47950eaa298d20e445d3520fce3
Justas <justasmereckas@gmail.com>
2017-10-08 19:32:45 +0100

    Changes

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello():
-    return "hwllo Justas "
+    return "Hello"
 
 @app.route('/clockin/<abc>')
 def clockin(abc):
-    return abc
+    return render_template("index.html",name=abc)
 
 if __name__ == '__main__':
     app.run(debug=True)
